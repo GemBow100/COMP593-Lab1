18d4e024962293dc2f0f2b3e2be1f06c1c320c76
Joelle Waugh <joelle.waugh@flemingcollege.ca>
2024-05-29 14:52:31 -0400

    Modified both files in the repo clone
    -Deleted a print() statement from script1.py
    - Added two print() statements to script2.py

--- Script1.py
+++ Script1.py
@@ -1,3 +1,2 @@
 # Print lines form Zen of Python
 print("Simple is better than complex.")
-print("Complex is better than complicated.")
--- Script2.py
+++ Script2.py
@@ -1,3 +1,4 @@
 # Print line from Zen of Python
 print(" Beautiful is better than ugly.")
 print("Explicit is better than implicit.")
+print("Flat is better than nested.")
