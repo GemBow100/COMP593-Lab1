c84a66f372cb2119f99acfe01e4028e3a74f9f3b
Joelle Waugh <joelle.waugh@flemingcollege.ca>
2024-05-29 14:31:23 -0400

    Revert "Modified both script files"
    This reverts commit 5a0ae21c9e8678a052ea5d92c255e2f97dd2a686.

--- Script1.py
+++ Script1.py
@@ -1,2 +1,3 @@
 # Print lines form Zen of Python
 print("Simple is better than complex.")
+print("Complex is better than complicated.")
--- Script2.py
+++ Script2.py
@@ -1,3 +1,2 @@
-# Print line from Zen of Python
 print(" Beautiful is better than ugly.")
 print("Explicit is better than implicit.")
